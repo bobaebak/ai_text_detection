d1ef6129b622f618f63d30ec844fd046dff41a29
bobae bak <zkd012@gmail.com>
2024-04-08 00:20:29 +0800

    add plot images

--- utils/plot_helper.py
+++ utils/plot_helper.py
@@ -3,42 +3,79 @@ from typing import Deque, List, Optional, Tuple
 from pydantic import BaseModel
 
 class PlotItem(BaseModel):
+    """
+    Data and its configuration to plot
+    """
     ptype: str="plot"
     xlabel: str=""
     ylabel: str=""
+    xlim: tuple=None 
+    ylim: tuple=None
     title: str=""
     x: list=None
     y: list=None
-    args: dict={}
+    args: List[dict]=None
 
 class PlotHelper(BaseModel):
+    """
+    configuration of in total plot
+    """
     row: int=1
     col: int=1
     figsize: tuple=(10, 10)
     title: str="" 
-    plots: Optional[List[PlotItem]] = None
+    plots: List[PlotItem]=None
 
 
-def draw_plots(plot_helper: PlotHelper):
-    fig, axs = plt.subplots(plot_helper.row, plot_helper.col, figsize=plot_helper.figsize)
+def draws(helper: PlotHelper):
+    fig, axs = plt.subplots(helper.row, helper.col, figsize=helper.figsize)
 
-    idx = 0
-    for r in range(plot_helper.row):
-        for c in range(plot_helper.col):
-            plot_type = {
-                "plot": axs[r, c].plot,
-                "hist": axs[r, c].hist,
+    iteration = iter(helper.plots)
+
+    if helper.row == 1 and helper.col == 1: # means only one plot
+        plot_branch = {
+            "plot": draw_plot,
+            "hist": draw_hist,
+            "bar": draw_bar,
+        }
+        
+        target = next(iteration) # choose data
+        plot_branch[target.ptype](axs, target) # choose accordingly relying on plot type
+        
+        axs.set(xlabel=target.xlabel, ylabel=target.ylabel)
+        axs.set_xlim(target.xlim) if target.xlim is not None else None
+        axs.set_ylim(target.ylim) if target.ylim is not None else None
+        axs.set_title(target.title)
+        axs.legend()
+
+
+    else:
+        for ax, target in zip(axs.flat, helper.plots):
+            plot_branch = {
+                "plot": draw_plot,
+                "hist": draw_hist,
+                "bar": draw_bar,
             }
+            target = next(iteration) # choose data
+            plot_branch[target.ptype](ax, target) # choose accordingly relying on plot type
             
-            plot_args_data = plot_helper.plots[idx]
-            plot_type[plot_args_data.ptype](x=plot_args_data.x, **plot_args_data.args)
-            axs[r, c].set_title(plot_args_data.title)
+            ax.set_xlim(target.xlim) if target.xlim is not None else None
+            ax.set_ylim(target.ylim) if target.ylim is not None else None
+            ax.set_title(target.title) 
+            ax.legend()
 
-            idx += 1
+            ax.set(xlabel=target.xlabel, ylabel=target.ylabel)
 
-    for ax, plot_args_data in zip(axs.flat, plot_helper.plots):
-        ax.set(xlabel=plot_args_data.xlabel, ylabel=plot_args_data.ylabel)
+        fig.suptitle(helper.title)
 
-    fig.suptitle(plot_helper.title)
+def draw_plot(ax, target):
+    for x, y, args in zip(target.x, target.y, target.args):
+        ax.plot(x, y, **args)
 
+def draw_hist(ax, target):
+    for data, args in zip(target.x, target.args):
+        ax.hist(x=data, **args)        
 
+def draw_bar(ax, target):
+    for x, y, args in zip(target.x, target.y, target.args):
+        ax.bar(x, y, **args)
