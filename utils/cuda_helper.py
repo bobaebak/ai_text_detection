ecf1db0bbee7485c0e27384ffee56e6b8b87f178
bobae bak <zkd012@gmail.com>
2024-05-26 15:19:48 +0800

    add gan-training process

--- utils/cuda_helper.py
+++ utils/cuda_helper.py
@@ -18,6 +18,15 @@ def print_gpu_devie_name():
         gpu_name = torch.cuda.get_device_name(i)
         print(f"cuda {i}: {gpu_name}")
 
+def print_total_gpu_memory():
+    # Get the device properties for the current GPU
+    device_props = torch.cuda.get_device_properties(torch.cuda.current_device())
+    print(device_props.name)
+
+    # Print the total GPU memory in gigabytes
+    total_gpu_memory = device_props.total_memory / (1024**3)
+    print(f"Total GPU memory: {total_gpu_memory:.2f} GB")
+
 def clear_gpu():
     import gc
     gc.collect()
